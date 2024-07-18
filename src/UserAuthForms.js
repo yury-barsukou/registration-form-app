9773e9eee366e76f6cf12252918dc7d905393733
Yury Barsukou <yury_barsukou@epam.com>
2024-07-18 11:22:38 +0300

    Change the order of forms

--- src/UserAuthForms.js
+++ src/UserAuthForms.js
@@ -4,28 +4,28 @@ import SignInForm from './SignInForm';
 import SignUpForm from './SignUpForm';
 
 const UserAuthForms = () => {
-  const [activeForm, setActiveForm] = useState('signup'); // 'signup' or 'signin'
+  const [activeForm, setActiveForm] = useState('signin'); // 'signin' or 'signup'
 
   return (
     <div className="user-entry">
       <h1 className="text-center main-header">
-        {activeForm === 'signup' ? 'Sign Up' : 'Sign In'}
+        {activeForm === 'signin' ? 'Sign Ip' : 'Sign Un'}
       </h1>
       <div className="form-switch">
-        <button
-          className={`switch-button ${activeForm === 'signup' ? 'active' : ''}`}
-          onClick={() => setActiveForm('signup')}
-        >
-          Sign Up
-        </button>
         <button
           className={`switch-button ${activeForm === 'signin' ? 'active' : ''}`}
           onClick={() => setActiveForm('signin')}
         >
           Sign In
         </button>
+        <button
+          className={`switch-button ${activeForm === 'signup' ? 'active' : ''}`}
+          onClick={() => setActiveForm('signup')}
+        >
+          Sign Up
+        </button>
       </div>
-      {activeForm === 'signup' ? <SignUpForm /> : <SignInForm />}
+      {activeForm === 'signin' ? <SignInForm /> : <SignUpForm />}
       <div className="privacy-policy text-center m-top-sm m-bottom-lg">
         <a className="legal-link" target="_blank" href="https://www.company.com/about-privacy-policy_US_AU_NZ_v10.html" rel="noopener noreferrer">
           Privacy Policy
