e86eedaf7c88a5a9e8484804af35ad9713dbbb00
Yury Barsukou <yury_barsukou@epam.com>
2024-07-19 10:20:01 +0300

    Create src/SignUpForm.test.js

---
+++ src/SignUpForm.test.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
+import SignUpForm from './SignUpForm';
+
+describe('SignUpForm', () => {
+  // Test cases will be added here
+});
