00dbc294195562ff50f3f6d820f79da2873f6f71
Yury Barsukou <yury_barsukou@epam.com>
2024-08-07 20:26:43 +0300

    Update SignInForm.test.js

--- src/test/SignInForm.test.js
+++ src/test/SignInForm.test.js
@@ -1,5 +1,5 @@
 import { render, screen, fireEvent } from '@testing-library/react';
-import SignInForm from './SignInForm';
+import SignInForm from '../SignInForm';
 
 describe('SignInForm', () => {
   beforeEach(() => {
