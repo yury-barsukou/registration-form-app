c8aeacd4d8c2b3c0b6a1bd2dc9aaa7a7ef315c60
Yury Barsukou <yury_barsukou@epam.com>
2024-12-05 18:47:55 +0300

    Update src/SignUpForm.test.js

--- src/SignUpForm.test.js
+++ src/SignUpForm.test.js
@@ -61,8 +61,8 @@ describe('SignUpForm', () => {
     test('validates password criteria correctly', () => {
       const password = screen.getByLabelText(LABELS.password);
       fireEvent.change(password, { target: { value: 'short' } });
-      expect(screen.getByText(/Minimum 8 characters/i).className).toMatch(/red/);
-      fireEvent.change(password, { target: { value: 'LongEnough1' } });
+      expect(screen.getByText(/Minimum 10 characters/i).className).toMatch(/red/);
+      fireEvent.change(password, { target: { value: 'LongEnough1!' } });
       expect(screen.getByText(/1 uppercase character/i).className).toMatch(/green/);
       expect(screen.getByText(/1 lowercase character/i).className).toMatch(/green/);
       expect(screen.getByText(/1 number/i).className).toMatch(/green/);
